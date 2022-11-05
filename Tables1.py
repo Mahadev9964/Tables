7c854f81658feb658b4ed5dee6ce7b352cd68baf
Mahadev9964 <117499007+Mahadev9964@users.noreply.github.com>
2022-11-05 10:06:09 +0530

    Add files via upload

---
+++ Tables1.py
@@ -0,0 +1,12 @@
+y=int(input("enter the table to display:"))
+z=int(input("enter the table to display:"))
+i=1
+j=1
+for i in range(i, 11):
+    prod1=y*i
+    a1=y
+    print(a1,i,prod1)
+for j in range(j, 11):
+    prod2=z*j
+    a1=z
+    print(a1,j,prod2)
